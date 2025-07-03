6ba5a487780c3a407cddaa9fbbb881d2338b1672
sirajulgit <sirajulali008@gmail.com>
2025-07-03 21:57:00 +0530

    blog

--- resources/views/user/blog.blade.php
+++ resources/views/user/blog.blade.php
@@ -3,58 +3,49 @@
 
 @section('content')
 
-    {{-- ############# | CMS BANNER | ############# --}}
-    <div class="banner-area position-relative">
-        <div class="banner-slide">
-            @foreach ($data['banner_data'] as $item)
-                <div>
-                    <img src="{{ asset($item['image']) }}">
-                </div>
-            @endforeach
-        </div>
 
+    <div class="inner-banner position-relative" style="background-image:url({{ asset('asset/frontend/images/banner.jpg')}});">
+        <div class="container">
+            <div class="text-center">
+                <strong> Blog </strong>
+            </div>
+        </div>
     </div>
 
 
 
 
-    <div class="blog-area pb-5 mt-5">
+
+
+    <!-- Service -->
+
+    <section class="gap-top gap-bottom">
         <div class="container">
-            <div class="about-content-right text-center mb-5 pb-4">
-                <h2> <strong>Our Blogs</strong> </h2>
-            </div>
 
-
-            {{-- ########### | Blogs | ########### --}}
             <div class="row">
                 @foreach ($data['blog_data'] as $item)
                     <div class="col-lg-4 mb-4">
-                        <div class="blog-card">
+                        <div class="blog-card matchHeight " style="height: 555px;">
                             <div class="blog-image">
                                 <img src="{{ asset($item['image']) }}">
                             </div>
                             <div class="blog-content">
-                                <span class="date-blog">
-                                    <i class="bi bi-calendar-check-fill"></i>
-                                    {{ \Carbon\Carbon::parse($item['created_at'])->format('F d, Y') }}
-                                </span>
-                                <h4> {{ $item['title'] }} </h4>
+                               <h4> {{ $item['title'] }} </h4>
+                               <p>
                                 @if ($item['short_description'])
                                     {!! \Illuminate\Support\Str::limit($item['short_description'], 100, '...') !!}
                                 @endif
-                            </div>
-                            <div class="readmore-btn">
-                                <a href="{{ route('blog_details', $item['slug']) }}"> Read More <i
-                                        class="bi bi-arrow-right"></i>
-                                </a>
+                                </p>
+                                <a class="btn-readmore" href="{{ route('blog_details', $item['slug']) }}"> Read More <i class="fa fa-arrow-right"></i> </a>
                             </div>
                         </div>
                     </div>
                 @endforeach
+
+
+
+
             </div>
-
-
-
             {{-- ########### | Pagination | ########### --}}
             @if ($data['blog_data']->hasPages())
                 <nav class="Page navigation pagination-area mt-4">
@@ -92,6 +83,9 @@
                 </nav>
             @endif
 
+
+
         </div>
-    </div>
+    </section>
+
 @endsection
