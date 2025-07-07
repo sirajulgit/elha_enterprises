563469de3055a4ba44c0a6fca7aba5512156ca92
sirajulgit <sirajulali008@gmail.com>
2025-07-07 22:38:45 +0530

    about2

--- resources/views/user/about_us.blade.php
+++ resources/views/user/about_us.blade.php
@@ -59,91 +59,34 @@
 
                 <div class="col-lg-7">
                     <div class="heading text-left mb-4">
-                        <h2> Frequently asked <br> questions </h2>
+                        <h2> {!! $homedata['faq']['head_title'] !!} </h2>
                         <span><img src="{{ asset('asset/frontend/images/three-dots.png') }}"> </span>
                     </div>
 
                     <div class="acor-area">
                         <div class="accordion" id="accordionExample">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingOne">
-                                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                        Efficient logistics solutions possible?
-                                    </button>
-                                </h2>
-                                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
-                                    data-bs-parent="#accordionExample">
-                                    <div class="accordion-body">
-                                        <strong>This is the first item's accordion body.</strong> It is shown by default,
-                                        until the collapse plugin adds the appropriate classes that we use to style each
-                                        element. These classes control the overall appearance, as well as the showing and
-                                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                                        our default variables. It's also worth noting that just about any HTML can go within
-                                        the <code>.accordion-body</code>, though the transition does limit overflow.
+                            @if (count($homedata['faq']['badge_data']) > 0)
+                                @foreach ($homedata['faq']['badge_data'] as $item)
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="heading{{ $item['id'] }}">
+                                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                                data-bs-target="#collapse{{ $item['id'] }}" aria-expanded="true"
+                                                aria-controls="collapse{{ $item['id'] }}">
+                                                {{ $item['badge_title_1'] }}
+                                            </button>
+                                        </h2>
+                                        <div id="collapse{{ $item['id'] }}" class="accordion-collapse collapse"
+                                            aria-labelledby="heading{{ $item['id'] }}"
+                                            data-bs-parent="#accordionExample">
+                                            <div class="accordion-body">
+                                                {{ $item['badge_details_1'] }}
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
+                                @endforeach
+                            @endif
 
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingTwo">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                        Focusing on medium tolarge-scale commercial?
-                                    </button>
-                                </h2>
-                                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                                    data-bs-parent="#accordionExample">
-                                    <div class="accordion-body">
-                                        <strong>This is the second item's accordion body.</strong> It is hidden by default,
-                                        until the collapse plugin adds the appropriate classes that we use to style each
-                                        element. These classes control the overall appearance, as well as the showing and
-                                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                                        our default variables. It's also worth noting that just about any HTML can go within
-                                        the <code>.accordion-body</code>, though the transition does limit overflow.
-                                    </div>
-                                </div>
-                            </div>
 
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingThree">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                        Add value to your business?
-                                    </button>
-                                </h2>
-                                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
-                                    data-bs-parent="#accordionExample">
-                                    <div class="accordion-body">
-                                        <strong>This is the third item's accordion body.</strong> It is hidden by default,
-                                        until the collapse plugin adds the appropriate classes that we use to style each
-                                        element. These classes control the overall appearance, as well as the showing and
-                                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                                        our default variables. It's also worth noting that just about any HTML can go within
-                                        the <code>.accordion-body</code>, though the transition does limit overflow.
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingFour">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                        Add value to your business?
-                                    </button>
-                                </h2>
-                                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
-                                    data-bs-parent="#accordionExample">
-                                    <div class="accordion-body">
-                                        <strong>This is the third item's accordion body.</strong> It is hidden by default,
-                                        until the collapse plugin adds the appropriate classes that we use to style each
-                                        element. These classes control the overall appearance, as well as the showing and
-                                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                                        our default variables. It's also worth noting that just about any HTML can go within
-                                        the <code>.accordion-body</code>, though the transition does limit overflow.
-                                    </div>
-                                </div>
-                            </div>
 
                         </div>
                     </div>
@@ -155,26 +98,41 @@
                         <div class="text-center mb-5">
                             <h3>Request A Quote</h3>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 mb-4">
-                                <input class="form-control" type="text" placeholder="Your Name">
+                        <form id="contact_form" method="POST">
+                            @csrf
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 mb-4">
+                                    <input class="form-control" type="text" placeholder="Your Name" name="name">
+                                    @if ($errors->has('name'))
+                                        <span class="form_error">{{ $errors->first('name') }}</span>
+                                    @endif
+                                </div>
+                                <div class="col-lg-6 col-md-6 mb-4">
+                                    <input class="form-control" type="email" placeholder="Your Email" name="email">
+                                     @if ($errors->has('email'))
+                                        <span class="form_error">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <div class="col-lg-6 col-md-6 mb-4">
+                                    <input class="form-control" type="tel" placeholder="Mobile Number" name="phone">
+                                     @if ($errors->has('phone'))
+                                        <span class="form_error">{{ $errors->first('phone') }}</span>
+                                    @endif
+                                </div>
+                                <div class="col-lg-6 col-md-6 mb-4">
+                                    <input class="form-control" type="text" placeholder="Subject">
+                                </div>
+                                <div class="col-lg-12 mb-4">
+                                    <textarea class="form-control" name="message"></textarea>
+                                     @if ($errors->has('message'))
+                                        <span class="form_error">{{ $errors->first('message') }}</span>
+                                    @endif
+                                </div>
+                                <div class="col-lg-12">
+                                    <input class="sub-btn" type="submit" value="Submit">
+                                </div>
                             </div>
-                            <div class="col-lg-6 col-md-6 mb-4">
-                                <input class="form-control" type="email" placeholder="Your Email">
-                            </div>
-                            <div class="col-lg-6 col-md-6 mb-4">
-                                <input class="form-control" type="text" placeholder="Subject">
-                            </div>
-                            <div class="col-lg-6 col-md-6 mb-4">
-                                <input class="form-control" type="tel" placeholder="Subject">
-                            </div>
-                            <div class="col-lg-12 mb-4">
-                                <textarea class="form-control"></textarea>
-                            </div>
-                            <div class="col-lg-12">
-                                <input class="sub-btn" type="submit" value="Submit">
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
 
@@ -245,14 +203,120 @@
         <div class="container">
 
             <ul class="d-flex justify-content-between align-items-center">
-                <li><img src="{{ asset('asset/frontend/images/sponcer-1.png') }}"></li>
-                <li><img src="{{ asset('asset/frontend/images/image-12.png') }}"></li>
-                <li><img src="{{ asset('asset/frontend/images/image-13.png') }}"></li>
-                <li><img src="{{ asset('asset/frontend/images/image-14.png') }}"></li>
-                <li><img src="{{ asset('asset/frontend/images/image-15.png') }}"></li>
+               <li><img src="{{ $homedata['client']['image_1'] }}"></li>
+                <li><img src="{{ $homedata['client']['image_2'] }}"></li>
+                <li><img src="{{ $homedata['client']['image_3'] }}"></li>
+                <li><img src="{{ $homedata['client']['image_4'] }}"></li>
+                <li><img src="{{ $homedata['client']['about_badge_icon1'] }}"></li>
             </ul>
 
         </div>
     </div>
 
+<div id="form-loader" style="display: none !important; position: fixed; top: 0; left: 0; width: 100%; height: 100%; 
+    background-color: rgba(0,0,0,0.4); z-index: 9999; display: flex; justify-content: center; align-items: center;">
+    <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+</div>
+
+@endsection
+@section('script_content')
+    <script>
+        $(document).ready(function() {
+
+            ////////////// form validation ////////////////////////
+            $('#contact_form').validate({
+                rules: {
+                    name: {
+                        required: true,
+                    },
+                    email: {
+                        required: true,
+                        email: true,
+                    },
+                    phone: {
+                        required: true,
+                        number: true,
+                    },
+                    message: {
+                        required: true
+                    },
+                },
+                messages: {
+                    name: "Please enter your name",
+                    email: {
+                        required: "Please enter your email",
+                        email: "Please enter a valid email address"
+                    },
+                    phone: {
+                        required: "Please enter your phone number",
+                        number: "Please enter a valid phone number"
+                    },
+                    message: "Please enter your message"
+                },
+                errorElement: 'span',
+                errorClass: 'form_error text-danger',
+                errorPlacement: function(error, element) {
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function(element) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function(element) {
+                    $(element).removeClass('is-invalid');
+                },
+                submitHandler: function(form, event) {
+                    event.preventDefault();
+
+                    const formData = new FormData(form);
+                    const url = "{{ route('post_contact_us') }}";
+
+                    // ++++++++++++++ | form submit | +++++++++++++++
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': "{{ csrf_token() }}"
+                        },
+                        url: url,
+                        method: "POST",
+                        data: formData,
+                        processData: false,
+                        contentType: false,
+                        beforeSend: function() {
+                         $('#form-loader').show();
+                            $(form).find('input[type="submit"]').prop('disabled', true);
+                        },
+                        success: function(response) {
+                            console.log(response);
+                            Swal.fire({
+                                icon: "success",
+                                title: "Success",
+                                text: "Thank you for contacting us.",
+                                showConfirmButton: true,
+                                confirmButtonText: "OK",
+                            });
+                        },
+                        error: function(error) {
+                           $('#form-loader').hide();
+                            console.log("error" + error);
+                            Swal.fire({
+                                icon: "error",
+                                title: "Error",
+                                text: "Something went wrong!",
+                                showConfirmButton: true,
+                                confirmButtonText: "OK",
+                            });
+                        },
+                        complete: function() {
+                         $('#form-loader').hide();
+                            form.reset();
+                            $(form).find('input[type="submit"]').prop('disabled', false);
+                        }
+                    });
+                }
+            });
+            ////////////// end form validation ////////////////////
+
+        });
+    </script>
 @endsection
