26524206ec102cdbb643462e1e5097c22c1bfaec
sirajulgit <sirajulali008@gmail.com>
2025-07-07 18:47:30 +0530

    home contact6

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -515,7 +515,7 @@
     </div>
 
 
-<div id="form-loader" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; 
+<div id="form-loader" style="display: none !important; position: fixed; top: 0; left: 0; width: 100%; height: 100%; 
     background-color: rgba(0,0,0,0.4); z-index: 9999; display: flex; justify-content: center; align-items: center;">
     <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;">
         <span class="visually-hidden">Loading...</span>
