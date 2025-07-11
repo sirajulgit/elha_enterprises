2a77141a3669e7c1a203a2e34a51402b20818278
Kuntal Pal <palkuntal36@gmail.com>
2025-07-11 15:05:17 +0530

    logo arroe

--- public/asset/frontend/js/custome.js
+++ public/asset/frontend/js/custome.js
@@ -96,8 +96,6 @@ jQuery(document).ready(function ($) {
         lazyLoad: 'progressive',
         arrows: true,
         dots: false,
-        prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
-        nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
     }).slickAnimation();
     $('.slick-nav').on('click touch', function (e) {
         e.preventDefault();
