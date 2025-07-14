3bc6e86ee70a2421bc879915c458517fc134b44f
sirajulgit <sirajulali008@gmail.com>
2025-07-14 23:19:37 +0530

    resource

--- resources/views/user/resource_details.blade.php
+++ resources/views/user/resource_details.blade.php
@@ -2,129 +2,45 @@
 
 
 @section('content')
- <div class="inner-banner position-relative" style="background-image:url({{ asset('asset/frontend/images/banner.jpg')}});">
-    <div class="container">
-      <div class="text-center">
-        <strong> Resources / {{ $data['resource_data']['title']}} </strong>
-      </div>
-    </div>
-  </div>
-
- 
- 
-
-  <section class="gap-top gap-bottom">
-    <div class="container">
-      
-      <div class="row">
-      
-
-        <div class="col-lg-12">
-          <div class="common-text gap-top">
-            
-
-
-              <h3>{{ $data['resource_data']['title']}}</h3>
-
-                <p>{!! $data['resource_data']['long_description'] !!}</p>
-
-          </div>
-        </div>
-
-
-        </div>
-      </div>
-
-         
-    </div>
-  </section>
-   <section class="faq-area gap-bottom gap-top">
+    <div class="inner-banner position-relative"
+        style="background-image:url({{ asset('asset/frontend/images/banner.jpg') }});">
         <div class="container">
-            <div class="row align-items-center">
-
-                <div class="col-lg-7">
-                    <div class="heading text-left mb-4">
-                        <h2> {!! $homedata['faq']['head_title'] !!} </h2>
-                        <span><img src="{{ asset('asset/frontend/images/three-dots.png') }}"> </span>
-                    </div>
-
-                    <div class="acor-area">
-                        <div class="accordion" id="accordionExample">
-                            @if (count($homedata['faq']['badge_data']) > 0)
-                                @foreach ($homedata['faq']['badge_data'] as $item)
-                                    <div class="accordion-item">
-                                        <h2 class="accordion-header" id="heading{{ $item['id'] }}">
-                                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                                data-bs-target="#collapse{{ $item['id'] }}" aria-expanded="true"
-                                                aria-controls="collapse{{ $item['id'] }}">
-                                                {{ $item['badge_title_1'] }}
-                                            </button>
-                                        </h2>
-                                        <div id="collapse{{ $item['id'] }}" class="accordion-collapse collapse"
-                                            aria-labelledby="heading{{ $item['id'] }}"
-                                            data-bs-parent="#accordionExample">
-                                            <div class="accordion-body">
-                                                {{ $item['badge_details_1'] }}
-                                            </div>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            @endif
+            <div class="text-center">
+                <strong> Resources / {{ $data['resource_data']['title'] }} </strong>
+            </div>
+        </div>
+    </div>
 
 
 
-                        </div>
-                    </div>
+
+    <section class="gap-top gap-bottom">
+        <div class="container">
+
+            <div class="row">
 
 
-                </div>
-                <div class="col-lg-5">
-                    <div class="from-area">
-                        <div class="text-center mb-5">
-                            <h3>Get in touch</h3>
-                        </div>
-                        <form id="contact_form" method="POST">
-                            @csrf
-                            <div class="row">
-                                <div class="col-lg-6 col-md-6 mb-4">
-                                    <input class="form-control" type="text" placeholder="Your Name" name="name">
-                                    @if ($errors->has('name'))
-                                        <span class="form_error">{{ $errors->first('name') }}</span>
-                                    @endif
-                                </div>
-                                <div class="col-lg-6 col-md-6 mb-4">
-                                    <input class="form-control" type="email" placeholder="Your Email" name="email">
-                                     @if ($errors->has('email'))
-                                        <span class="form_error">{{ $errors->first('email') }}</span>
-                                    @endif
-                                </div>
-                                <div class="col-lg-6 col-md-6 mb-4">
-                                    <input class="form-control" type="tel" placeholder="Mobile Number" name="phone">
-                                     @if ($errors->has('phone'))
-                                        <span class="form_error">{{ $errors->first('phone') }}</span>
-                                    @endif
-                                </div>
-                                <div class="col-lg-6 col-md-6 mb-4">
-                                    <input class="form-control" type="text" placeholder="Subject">
-                                </div>
-                                <div class="col-lg-12 mb-4">
-                                    <textarea class="form-control" name="message"></textarea>
-                                     @if ($errors->has('message'))
-                                        <span class="form_error">{{ $errors->first('message') }}</span>
-                                    @endif
-                                </div>
-                                <div class="col-lg-12">
-                                    <input class="sub-btn" type="submit" value="Submit">
-                                </div>
-                            </div>
-                        </form>
+                <div class="col-lg-12">
+                    <div class="common-text gap-top">
+
+
+
+                        <h3>{{ $data['resource_data']['title'] }}</h3>
+
+                        <p>{!! $data['resource_data']['long_description'] !!}</p>
+
                     </div>
                 </div>
+
 
             </div>
         </div>
+
+
+        </div>
     </section>
-  
+    
+
 @endsection
 @section('script_content')
     <script>
@@ -188,7 +104,7 @@
                         processData: false,
                         contentType: false,
                         beforeSend: function() {
-                         $('#form-loader').show();
+                            $('#form-loader').show();
                             $(form).find('input[type="submit"]').prop('disabled', true);
                         },
                         success: function(response) {
@@ -202,7 +118,7 @@
                             });
                         },
                         error: function(error) {
-                           $('#form-loader').hide();
+                            $('#form-loader').hide();
                             console.log("error" + error);
                             Swal.fire({
                                 icon: "error",
@@ -213,7 +129,7 @@
                             });
                         },
                         complete: function() {
-                         $('#form-loader').hide();
+                            $('#form-loader').hide();
                             form.reset();
                             $(form).find('input[type="submit"]').prop('disabled', false);
                         }
