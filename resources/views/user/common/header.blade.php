fafc4f65cba1aa38f2611eb050025c4cf088e264
sirajulgit <sirajulali008@gmail.com>
2025-07-20 00:43:07 +0530

    footer3

--- resources/views/user/common/header.blade.php
+++ resources/views/user/common/header.blade.php
@@ -10,8 +10,12 @@
                    <ul class="d-flex social-media">
                        <li><a href="{{ $settings['fb_link'] }}"> <i class="fa fa-facebook"></i> </a></li>
                        <li><a href="{{ $settings['twitter_link'] }}"> <i class="fa fa-twitter"></i> </a></li>
-                       <li><a href="{{ $settings['insra_link'] }}"> <i class="fa fa-linkedin"></i> </a></li>
+                       <li><a href="{{ $settings['insra_link'] }}"> <i class="fa fa-instagram"></i> </a></li>
+                        <li><a href="{{ $settings['insra_link'] }}"> <i class="fa fa-linkedin"></i> </a></li>
+                       
                        <li><a href="{{ $settings['youtube_link'] }}"> <i class="fa fa-youtube-play"></i> </a></li>
+                       <li><a href="{{ $settings['youtube_link'] }}"> <i class="fa-brands fa-tiktok"></i> </a></li>
+                       
                    </ul>
                    <a class="mail-head" href="mailto:{{ $settings['contact_email'] }}"><i class="fa fa-envelope-o me-2"></i>
                        {{ $settings['contact_email'] }}</a>
